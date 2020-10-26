955cca7826d780058ee1d2a5ffc00feb7eb76004
Stewart Johnston <johnstons1@student.ncmich.edu>
2020-10-26 17:31:48 -0400

    Basic requirements met

--- project_1/src/javascript/project_1.js
+++ project_1/src/javascript/project_1.js
@@ -7,7 +7,7 @@
 
 const rlsync = require('readline-sync');
 
-const DEBUG = true;
+const DEBUG = false;
 const DEFAULT_RETURN = 'Default return';
 //should a calling function have a default value, use that
 
@@ -18,21 +18,129 @@ const GARAGE_PRICE_PER_CAR = 6000;
 
 function main()
 {
-	let current_lot = new Map([
-		['lot_num',],
-		['num_bedrooms',],
-		['num_bathrooms',],
-		['garage_capacity',],
-	]);
-	calc_lot_price(current_lot);
 
-	prompt_int_in_range();
+	let lots = get_lots();
+
+	do
+	{
+		let current_lot = new Map([
+			['lot_num',
+				( Math.max(...lots.map(lot => lot.get('lot_num'))) +1 )],
+			['num_bedrooms',],
+			['num_bathrooms',],
+			['garage_capacity',],
+		]);
+
+
+		current_lot.set('num_bedrooms', prompt_bedrooms());
+		current_lot.set('num_bathrooms', prompt_bathrooms());
+		current_lot.set('garage_capacity', prompt_garage());
+
+		let lot_price = calc_lot_price(Object.fromEntries(current_lot));
+		if (DEBUG) console.log(`!! lot_price: ${lot_price}`);
+
+		current_lot.set('lot_price', lot_price);
+
+		lots.push(current_lot);
+	} while( true == prompt_continue('Process another lot? [Y]es or [N]'));
+
+	print_lots(lots);
 }
+
+function print_lots(lots)
+{
+	if (lots.length <= 1)
+	{
+		return Error('Lots array has too few lots in it!');
+	}
+	for (const lot of lots)
+	{
+		if (lot.get('lot_num') == 0)
+		{
+			continue;
+		}
+
+		console.log(`lot number: ${lot.get('lot_num')}, price: \
+${lot.get('lot_price')}`);
+	}
+}
+
+function get_lots()
+{
+	return [new Map([['lot_num', 0]])];
+}
+
+function prompt_bedrooms()
+{
+	let bedroom_message = `Choose the number of bedrooms you'd like. The \
+price is ${BEDROOM_PRICE}, and this will default to 1: `;
+	let result = prompt_int_in_range(bedroom_message, 1, 0);
+
+	if (DEBUG) console.log(`!! bedrooms: ${result}, isNaN?: ${isNaN(result)}`);
+
+	if (DEBUG) console.log(`!! Default return triple-eq result: \
+${DEFAULT_RETURN === result}`);
+
+	if (DEFAULT_RETURN === result)
+	{
+		return 1;
+	}
+
+	return result;
+}
+
+function prompt_bathrooms()
+{
+	let bathroom_message = `Choose the number of bathrooms you'd like. The \
+price is ${BATHROOM_PRICE}, and this will default to 1: `;
+	let result = prompt_int_in_range(bathroom_message, 1, 0);
+
+	if (DEBUG) console.log(`!! bathrooms: ${result}, isNaN?: ${isNaN(result)}`);
+
+	if (DEBUG) console.log(`!! Default return triple-eq result: \
+${DEFAULT_RETURN === result}`);
+	if (DEFAULT_RETURN === result)
+	{
+		return 1;
+	}
+
+	return result;
+}
+
+function prompt_garage()
+{
+	let garage_message = `Choose the capacity of the garage you'd like. The \
+price is ${GARAGE_PRICE_PER_CAR}, and this will default to 1: `;
+	let result = prompt_int_in_range(garage_message, 1, 0);
+
+	if (DEBUG) console.log(`!! garage_capacity: ${result}, isNaN?: \
+${isNaN(result)}`);
+
+	if (DEBUG) console.log(`!! Default return triple-eq result: \
+${DEFAULT_RETURN === result}`);
+	if (DEFAULT_RETURN === result)
+	{
+		return 1;
+	}
+
+	return result;
+}
+
 
 function calc_lot_price({num_bedrooms, num_bathrooms, garage_capacity})
 {
 	let lot_price = BASE_PRICE;
 	if(DEBUG) console.log(`lot_price: ${lot_price}`);
+
+	if (DEBUG)
+	{
+		console.log(`!! num_bedrooms: ${num_bedrooms}, \
+isNaN?: ${isNaN(num_bedrooms)}`);
+		console.log(`!! num_bathrooms: ${num_bathrooms}, \
+isNaN?: ${isNaN(num_bathrooms)}`);
+		console.log(`!! garage_capacity: ${garage_capacity}, \
+isNaN?: ${isNaN(garage_capacity)}`);
+	}
 
 	lot_price += (BEDROOM_PRICE * num_bedrooms);
 	if(DEBUG) console.log(`lot_price w/ bedrooms: ${lot_price}`);
@@ -48,16 +156,17 @@ function calc_lot_price({num_bedrooms, num_bathrooms, garage_capacity})
 
 function prompt_int_in_range(message_str, min_num, max_num)
 {
-	let valid_min_max = validate_num_in_range(min_num, max_num, 0);
+	let valid_min_max = validate_num_in_range(min_num, max_num, min_num);
 	if (DEBUG)
 	{
-		console.log(`min_num: ${min_num}, max_num: ${max_num}, valid_min_max:
-			${valid_min_max}`);
+		console.log(`!! min_num: ${min_num}, \
+max_num: ${max_num}, \
+valid_min_max: ${valid_min_max}`);
 	}
 	if (typeof(valid_min_max) == Error) return valid_min_max;
 
-	let limit_message = `Input a number between ${min_num} and ${max_num}. A
-	maximum of 0 implies no limit.`;
+	let limit_message = `Input a number between ${min_num} and ${max_num}. \
+A maximum of 0 implies no limit.`;
 
 	let result;
 	let valid_result = false;
@@ -65,17 +174,18 @@ function prompt_int_in_range(message_str, min_num, max_num)
 	{
 		result = rlsync.questionInt(message_str,
 			{limitMessage: limit_message});
-		if (DEBUG) console.log(`result: ${result}`);
+		if (DEBUG) console.log(`!! result: ${result}`);
 
 		valid_result = validate_num_in_range(min_num, max_num, result);
-		if (DEBUG) console.log(`valid_result: ${valid_result}`);
+		if (DEBUG) console.log(`!! valid_result: ${valid_result}`);
 
 		if (typeof(valid_result) == Error || true != valid_result) 
 		{ 
 			console.log(limit_message);
 			//shouldn't reach this for an error, because rlsync.questionInt
 			//checks for valid integers
-			if (prompt_continue('Try again? [Y]es or [N]o') != false)
+			if (prompt_continue('Choose a new number [y], or use default ' +
+				'[any other key]?') == false)
 			{
 				return DEFAULT_RETURN;
 			}
