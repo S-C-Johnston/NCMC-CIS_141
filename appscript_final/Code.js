5d32e5c2fe585ad776f028c60a2d3171d2b545da
Stewart Johnston <johnstons1@student.ncmich.edu>
2025-12-10 20:10:43 -0500

    Change all Logger calls to console.log

--- appscript_final/Code.js
+++ appscript_final/Code.js
@@ -3,7 +3,7 @@ function cleanUp() {
 	var delayDays = 10 // Enter # of days before messages are moved to trash
 	var maxDate = new Date();
 	maxDate.setDate(maxDate.getDate() - delayDays);
-	Logger.log("Maximum date is: %s", maxDate);
+	console.log("Maximum date is: %s", maxDate);
 
 	const label_regexp = /delete me.*/gi;
 	var label_names = match_label(label_regexp);
@@ -11,11 +11,11 @@ function cleanUp() {
 	for (var i = 0; i < label_names.length; i++) {
 		var label = GmailApp.getUserLabelByName(label_names[i]);
 		var threads = label.getThreads();
-		Logger.log("Handling %s label", label);
+		console.log("Handling '%s' label", label.getName());
 		for (var j = 0; j < threads.length; j++) {
 			if (threads[j].getLastMessageDate() < maxDate) {
-				Logger.log(
-					"Deleting message with subject %s and date %s",
+				console.log(
+					"Trashing message with subject %s and date %s",
 					threads[j].getFirstMessageSubject(),
 					threads[j].getLastMessageDate()
 				);
@@ -23,7 +23,7 @@ function cleanUp() {
 					threads[j].moveToTrash();
 				}
 				else {
-					Logger.log("Debugging enabled, no deletion")
+					console.log("Debugging enabled, no trashing")
 				}
 			}
 		}
@@ -39,7 +39,7 @@ function match_label(re) {
 	}
 	for (var i = 0; i < names.length; i++) {
 		if (names[i].match(re)) {
-			Logger.log("label: %s added", names[i]);
+			console.log("label: %s added", names[i]);
 			results.push(names[i]);
 		};
 	}
